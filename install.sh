c06b3a59c7fa0ee9355d210cdeb85f962eeac94c
Harrisonust <harrisonlo2024@hps-program.com>
2024-08-05 21:23:33 +0800

    respeaker setup and test

--- install.sh
+++ install.sh
@@ -8,5 +8,10 @@ sudo apt install -y git python3-pip python3-venv i2c-tools
 python3 -m venv .venv
 source .venv/bin/activate
 
-pip install rpi.gpio smbus spidev adafruit-blinka adafruit-circuitpython-ssd1306 adafruit-circuitpython-framebuf
+pip install rpi.gpio smbus spidev pyaudio pillow
 
+# respeaker
+git clone https://github.com/respeaker/seeed-voicecard ~/seeed-voicecard/
+cd ~/seeed-voicecard
+sudo ./install.sh
+sudo reboot
