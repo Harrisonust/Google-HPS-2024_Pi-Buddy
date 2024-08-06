9d725ac6727f8c645438bb247df4486646a84fe1
Harrisonust <harrisonlo2024@hps-program.com>
2024-08-06 23:20:40 +0800

    send screen update once when update is called

--- src/components/st7735s/st7735s.py
+++ src/components/st7735s/st7735s.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import spidev
 import RPi.GPIO as GPIO
 from st7735s_reg import *
+import numpy as np
 
 class RGB565Color:
     # https://rgbcolorpicker.com/565
@@ -36,6 +37,12 @@ class Screen:
         self._background_color = RGB565Color.BLACK
         self._chunksize        = 1024
 
+        self._buf = np.zeros([self._row_dim, self._col_dim, 2], dtype=np.uint8)
+        self._prev_buf = np.zeros_like(self._buf)
+
+        self._last_update = time.time()
+        self._fps = 0.0
+
         GPIO.setup(self._pin_dc, GPIO.OUT)
         GPIO.setup(self._pin_rst, GPIO.OUT)
         if self._pin_cs is not None:
@@ -53,20 +60,17 @@ class Screen:
         time.sleep(0.1)
 
     def _init_display(self) -> None:
-        self.set_sw_reset()  # Software reset
+        self.set_sw_reset()         # Software reset
         time.sleep(0.15)
 
-        self.set_sleep_control(1)  # Exit sleep mode
+        self.set_sleep_control(1)   # Exit sleep mode
         time.sleep(0.5)
         
         self.set_color_mode(1)
         
-        self._write_command(MADCTL)  # Memory access control
-        self._write_data([0xC8])
-        
         self.set_inversion(0)
         
-        self.set_display_mode(1)  # Normal display mode
+        self.set_display_mode(1)    # Normal display mode
         
         self.set_display_on_off(1)
         time.sleep(0.1)
@@ -88,18 +92,18 @@ class Screen:
             GPIO.output(self._pin_cs, GPIO.HIGH)
     
     def _set_address(self, row_col: int, from_addr: int, to_addr: int) -> None:
-        if row_col == 0:   # row
-            self._write_command(RASET)
-        elif row_col == 1: # col
+        if row_col == 0:   # col
             self._write_command(CASET)
+        elif row_col == 1: # row
+            self._write_command(RASET)
         else:
-            raise ValueError('row = 0, col = 1')
+            raise ValueError('col = 0, row = 1')
         
         self._write_data([from_addr >> 8, from_addr & 0xFF, to_addr >> 8, to_addr & 0xFF])
 
     def _set_area(self, x0, y0, x1, y1):
-        self._set_address(1, x0, x1)
-        self._set_address(0, y0, y1)
+        self._set_address(0, x0+2, x1+2) # col
+        self._set_address(1, y0+1, y1+1) # row
         self._write_command(RAMWR)
 
     def get_col_dim(self) -> int:
@@ -161,13 +165,14 @@ class Screen:
         self._background_color = color
     
     def draw_pixel(self, x, y, color=None) -> None:
-        if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
-            raise ValueError("pixel out of bound")
+        if x < 0 or x >= self._col_dim or y < 0 or y >= self._row_dim:
+            #raise ValueError("pixel out of bound")
+            return
 
         if color is None:
             color = self._brush_color
-        self._set_area(x, y, x, y)
-        self._write_data([color >> 8, color & 0xFF])
+
+        self._buf[y, x] = [color >> 8, color & 0xFF]
 
     def draw_vertical_line(self, x, y, len_, color=None) -> None:
         if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
@@ -177,8 +182,8 @@ class Screen:
 
         if color is None:
             color = self._brush_color
-        self._set_area(x, y, x, y+len_-1)
-        self._write_data([color >> 8, color & 0xFF] * len_)
+        for i in range(len_):
+           self.draw_pixel(x, y+i, color)
 
     def draw_horizontal_line(self, x, y, len_, color=None) -> None:
         if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
@@ -188,8 +193,9 @@ class Screen:
 
         if color is None:
             color = self._brush_color
-        self._set_area(x, y, x+len_-1, y)
-        self._write_data([color >> 8, color & 0xFF] * len_) 
+
+        for i in range(len_):
+            self.draw_pixel(x+i, y, color)
 
     def draw_rectangle(self, x, y, xlen, ylen, color=None) -> None:
         if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
@@ -199,9 +205,9 @@ class Screen:
 
         if color is None:
             color = self._brush_color
-        self._set_area(x, y, x+xlen-1, y+ylen-1)
-        for _ in range(ylen):
-            self._write_data([color >> 8, color & 0xFF] * xlen)
+        
+        for i in range(ylen):
+            self.draw_horizontal_line(x, y+i, xlen, color)
 
     def draw_circle(self, x, y, radius, color=None)->None:
         if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
@@ -209,16 +215,10 @@ class Screen:
         if color is None:
             color = self._brush_color
         
-        buf = [[self._background_color >> 8, self._background_color & 0xFF] * (2*radius) for _ in range(2*radius)]
-        
-        self._set_area(x-radius+1, y-radius+1, x+radius, y+radius)
-        
         for j in range(-radius, radius):
             for i in range(-radius, radius):
                 if i**2 + j**2 < radius**2:
-                    buf[j+radius][(i+radius)*2] = color >> 8
-                    buf[j+radius][(i+radius)*2+1] = color & 0xFF
-            self._write_data(buf[j+radius])
+                    self.draw_pixel(x+i, y+j, color)
 
     def draw_sector(self, x, y, radius, start_angle, end_angle, color=None) -> None:
         if x < 0 or x > self._col_dim or y < 0 or y > self._row_dim:
@@ -229,10 +229,6 @@ class Screen:
         start_angle = start_angle % 360
         end_angle   = end_angle % 360
 
-        buf = [[self._background_color >> 8, self._background_color & 0xFF] * (2 * radius) for _ in range(2 * radius)]
-
-        self._set_area(x - radius + 1, y - radius + 1, x + radius, y + radius)
-
         for j in range(-radius, radius):
             for i in range(-radius, radius):
                 if i**2 + j**2 < radius**2:
@@ -240,36 +236,31 @@ class Screen:
                     angle = angle + 360 if angle < 0 else angle  # Normalize angle to [0, 360)
                     if start_angle <= end_angle:
                         if start_angle <= angle <= end_angle:
-                            buf[j + radius][(i + radius) * 2] = color >> 8
-                            buf[j + radius][(i + radius) * 2 + 1] = color & 0xFF
+                            self.draw_pixel(x+i, y+j, color)
                     else:  # Crossing the 0-degree line
                         if angle >= start_angle or angle <= end_angle:
-                            buf[j + radius][(i + radius) * 2] = color >> 8
-                            buf[j + radius][(i + radius) * 2 + 1] = color & 0xFF
-            
-            self._write_data(buf[j + radius])
+                            self.draw_pixel(x+i, y+j, color) 
 
     def draw_text(self, x, y, text, size, color=None) -> None:
+        if x < 0 or x >= self._col_dim or y < 0 or y >= self._row_dim:
+            raise ValueError("Pixel out of bound")
+
         if color is None:
             color = self._brush_color
 
         font = ImageFont.truetype("./arial.ttf", size)
-        image = Image.new("RGB", (self._col_dim, self._row_dim), (0, 0, 0))
-        draw = ImageDraw.Draw(image)
-        draw.text((x, y), text, font=font, fill=0xFFFFFF)
+        img  = Image.new("RGB", (len(text)*(size-4), size+4), (0, 0, 0))
+        draw = ImageDraw.Draw(img)
+        draw.text((0, 0), text, font=font, fill=0xFFFFFF)
 
-        image = image.convert("RGB")
-        pixels = list(image.getdata())
-        buf = []
-        for pixel in pixels:
-            r, g, b = pixel
-            rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-            buf.append(rgb565 >> 8)
-            buf.append(rgb565 & 0xFF)
-
-        self._set_area(x, y, x+len(text)*size-1, y+size-1)
-        for i in range(0, len(buf), self._chunksize):
-            self._write_data(buf[i:i+self._chunksize])
+        img = img.convert("RGB")
+        pixels = list(img.getdata())
+        for j in range(img.height):
+            for i in range(img.width):
+                r, g, b = pixels[j*img.width+i]
+                rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
+                if rgb565 != 0x0000:
+                    self.draw_pixel(x+i, y+j, rgb565)
 
     def draw_image(self, x, y, width, height, path) -> None:
         if x < 0 or x >= self._col_dim or y < 0 or y >= self._row_dim:
@@ -282,16 +273,11 @@ class Screen:
         img = img.convert("RGB")
 
         pixels = list(img.getdata())
-        buf = []
-        for pixel in pixels:
-            r, g, b = pixel
-            rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-            buf.append(rgb565 >> 8)
-            buf.append(rgb565 & 0xFF)
-        
-        self._set_area(x, y, x+width-1, y+height-1)
-        for i in range(0, len(buf), self._chunksize):
-            self._write_data(buf[i:i+self._chunksize])
+        for j in range(height):
+            for i in range(width):
+                r, g, b = pixels[j*width+i]
+                rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
+                self.draw_pixel(x+i, y+j, rgb565)
 
     def fill_screen(self, color) -> None:
         if color is None:
@@ -299,31 +285,52 @@ class Screen:
         self.draw_rectangle(0, 0, self._col_dim, self._row_dim, color)
 
     def clear(self) -> None:
-        self.fill_screen(0x0000)
+        self._buf = np.zeros((self._row_dim, self._col_dim, 2), dtype=np.uint8)
+
+    def update(self) -> None:
+        self._fps = 1/(time.time()-self._last_update)
+        self._set_area(0, 0, self._col_dim-1, self._row_dim-1)
+        for i in range(0, len(self._buf.flatten()), self._chunksize):
+            self._write_data(self._buf.flatten()[i:i+self._chunksize].tolist())
+        self._last_update = time.time()
+
+    def get_fps(self) -> float:
+        return self._fps
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    screen = Screen(col_dim=132, row_dim=132)
+    screen = Screen(col_dim=128, row_dim=160)
     screen.clear()
 
     while 1:
-        screen.draw_pixel(0, 0)
+        screen.draw_pixel(0, 0, RGB565Color.WHITE)
+        screen.draw_pixel(1, 0, RGB565Color.WHITE)
+        screen.draw_pixel(0, 1, RGB565Color.WHITE)
+        screen.draw_pixel(1, 1, RGB565Color.WHITE)
         screen.draw_pixel(0, screen.get_row_dim()-1)
         screen.draw_pixel(screen.get_col_dim()-1, 0)
         screen.draw_pixel(screen.get_col_dim()-1, screen.get_row_dim()-1)
-
+        
         screen.draw_vertical_line(15, 15, 10)
         screen.draw_horizontal_line(30, 10, 20)
-        screen.draw_circle(100, 20, 10, RGB565Color.YELLOW)
-        screen.draw_image(35, 35, 60, 60, "./google.jpg")
-        screen.draw_text(40, 100, "Hello", 14, RGB565Color.WHITE)
+        screen.draw_rectangle(10, 35, 20, 20, RGB565Color.PINK)
+        
+        screen.draw_circle(90, 20, 20, RGB565Color.BLUE)
+        screen.draw_sector(40, 30, 25, -45, 45, RGB565Color.ORANGE)
+        
+        screen.draw_image(44, 64, 40, 40, "./google.jpg")
+        screen.draw_text(20, 120, "Google HPS 2024", 12, RGB565Color.WHITE)
 
         colorlist = [RGB565Color.BLACK, RGB565Color.WHITE, RGB565Color.BLUE, RGB565Color.RED, 
                      RGB565Color.GREEN, RGB565Color.ORANGE, RGB565Color.YELLOW, RGB565Color.PINK, 
                      RGB565Color.CYAN, RGB565Color.VIOLET]
         for index, color in enumerate(colorlist):
-            screen.draw_rectangle(10+index*10, 124, 10, 8, color)
+            screen.draw_rectangle(10+index*10, 151, 10, 8, color)
+        screen.draw_horizontal_line(10, 151, 100, RGB565Color.WHITE)
+        screen.draw_horizontal_line(10, 159, 100, RGB565Color.WHITE)
+        screen.draw_vertical_line(10, 151, 9, RGB565Color.WHITE)
+        screen.draw_vertical_line(110, 151, 9, RGB565Color.WHITE)
 
-    
-        time.sleep(1)
+        screen.draw_text(80, 0, "FPS: {:.2f}".format(screen.get_fps()), 10, RGB565Color.CYAN)
+        screen.update()
         screen.clear()
