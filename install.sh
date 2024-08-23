e1beb186661dd33363e0ecbd0f01ea10c97fe543
Harrisonust <harrisonlo2024@hps-program.com>
2024-08-24 00:33:23 +0800

    update install script, test passed with python 3.11.2

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# this install script was tested with python 3.11.2
+
 sudo apt update 
 sudo apt upgrade -y
 
-sudo apt install -y git python3-pip python3-venv i2c-tools libopenjp2-7 libglib2.0-0 libopenblas-dev python3-picamera2 python3-opencv
+sudo apt remove python3-rpi.gpio
+
+sudo apt install -y python3-rpi-lgpio
+sudo apt install -y git python3-pip python3-venv python3-rpi-lgpio i2c-tools 
+sudo apt install -y python3-picamera2 --no-install-recommends
 
 python3 -m venv .venv
 source .venv/bin/activate
 
-pip install rpi.gpio smbus spidev pillow numpy requests
+pip install -r requirements.txt
 
