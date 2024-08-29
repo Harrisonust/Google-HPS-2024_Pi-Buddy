46c9f5ca16f7315b34e605a21f33032e7c504f67
Harrisonust <harrisonlo2024@hps-program.com>
2024-08-29 20:29:58 +0800

    merge and test code

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -2,11 +2,12 @@ from .battery_handler import BatteryHandler
 from .encoders_handler import EncodersHandler
 from .test_encoders_handler import TestEncodersHandler
 from .menu_screen_handler import MenuScreenHandler
-
+from .audio_handler import AudioHandler
 
 __all__ = [
     'BatteryHandler',
     'TestEncodersHandler',
     'EncodersHandler',
-    'MenuScreenHandler'
+    'MenuScreenHandler',
+    'AudioHandler'
 ]
