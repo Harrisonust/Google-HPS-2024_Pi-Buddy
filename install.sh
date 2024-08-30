5c5d26733c2f9c4511c8b85b2249940da657649c
TiasheKnight <115419422+TiasheKnight@users.noreply.github.com>
2024-08-30 12:05:10 +0800

    update requirements

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ sudo apt upgrade -y
 
 sudo apt remove python3-rpi.gpio
 
-sudo apt install -y git python3-pip python3-venv python3-rpi-lgpio i2c-tools sqlite3 
+sudo apt install -y git python3-pip python3-venv python3-rpi-lgpio i2c-tools sqlite3 mpg321 libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
 sudo apt install -y python3-picamera2 --no-install-recommends
 
-python3 -m venv .venv
+python3 -m venv --system-site-packages .venv 
+python3 -m pip install pyaudio
 source .venv/bin/activate
 
-pip install -r requirements.txt
-
+pip3 install -r requirements.txt
