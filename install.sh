92287bfcd5aacfc40a04cd855246ee0d7b31f6be
Harrisonust <harrisonlo2024@hps-program.com>
2024-09-01 03:37:07 +0800

    update install script: add respeaker driver and configs

--- install.sh
+++ install.sh
@@ -7,11 +7,55 @@ sudo apt upgrade -y
 
 sudo apt remove python3-rpi.gpio
 
+<<<<<<< Updated upstream
 sudo apt install -y git python3-pip python3-venv python3-rpi-lgpio i2c-tools sqlite3 mpg321 libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg
+=======
+sudo apt install -y git python3-pip python3-venv python3-rpi-lgpio i2c-tools sqlite3 mpg321 libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg python3-pyaudio flac python3-espeak espeak
+
+>>>>>>> Stashed changes
 sudo apt install -y python3-picamera2 --no-install-recommends
 
 python3 -m venv --system-site-packages .venv 
-python3 -m pip install pyaudio
 source .venv/bin/activate
 
 pip3 install -r requirements.txt
+
+source ./respeaker_install.sh 
+
+# mic hat with audio examples
+git clone git@github.com:respeaker/mic_hat.git ~/mic_hat
+
+sudo echo "defaults.pcm.rate_converter "samplerate"
+
+pcm.!default {
+    type asym
+    playback.pcm "playback"
+    capture.pcm "capture"
+}
+
+pcm.playback {
+    type plug
+    slave.pcm "dmixed"
+}
+
+pcm.capture {
+    type plug
+    slave.pcm "mono_capture"
+}
+
+pcm.dmixed {
+    type dmix
+    slave.pcm "hw:seeed2micvoicec"
+    ipc_key 555555
+}
+
+pcm.mono_capture {
+    type route
+    slave {
+        pcm "hw:seeed2micvoicec"
+        channels 1
+    }
+    ttable.0.0 1
+    ttable.1.0 0
+}" > /etc/asound.conf
+
---
+++ respeaker_install.sh
@@ -0,0 +1,4 @@
+git clone -b v6.6 https://github.com/HinTak/seeed-voicecard.git ~/seeed-voicecard
+cd ~/seeed-voicecard
+git checkout v6.6 # this step is important or else the respeaker can not record/play any audio file
+sudo ./install.sh
